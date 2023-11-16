94b1b3b26d06c7af6b8fcec601848ec2acc0eb87
sajib hasan <sajibhasan800080@gmail.com>
2023-11-17 00:25:43 +0600

    solved problem 3 & added ouptut 3

--- assignment-3.c
+++ assignment-3.c
@@ -28,38 +28,87 @@ int main(){
 
 
 //    problem: 2.Write a program in C to count the total number of duplicate elements in an array and print both duplicate values and the unique values.
-      int n, counter = 0;
-      printf("Input the number to be stored in the array: ");
-      scanf("%d", &n);
+//      int n, counter = 0;
+//      printf("Input the number to be stored in the array: ");
+//      scanf("%d", &n);
+//
+//      int myArray[n];
+//
+//      for(int i = 0; i < n; i++){
+//        printf("myArray[%d] : ", i);
+//        scanf("%d", &myArray[i]);
+//      }
+//
+//      printf("\nArray: ");
+//      for(int i = 0; i < n; i++){
+//        printf("%d ", myArray[i]);
+//      }
+//
+//
+//
+//      printf("\n");
+//      printf("duplicate elements: ");
+//      for(int i = 0; i < n; i++){
+//        for(int j = i + 1; j < n; j++){
+//            if(myArray[i] == myArray[j]){
+//                printf("%d ", myArray[i]);
+//                counter++;
+//                break;
+//            }
+//            if(myArray[i] != myArray[j]){
+//
+//            }
+//        }
+//      }
+//
+//      printf("\n\nTotal number of duplicate elements found in the array  is: %d\n", counter);
+//
+//      printf("\n");
 
-      int myArray[n];
 
-      for(int i = 0; i < n; i++){
+
+
+
+
+
+
+//  problem 3: Write a program in C to find the maximum and minimum elements in an array.
+    int n, min,max;
+    printf("Input the number of elements to store in the array: ");
+    scanf("%d", &n);
+
+    int myArray[n];
+
+    for(int i = 0; i < n; i++){
         printf("myArray[%d] : ", i);
         scanf("%d", &myArray[i]);
-      }
+    }
 
-
-      printf("\nArray : ");
-      for(int i = 0; i < n; i++){
+    printf("\n\nArrays are: ");
+    for(int i = 0; i < n; i++){
         printf("%d ", myArray[i]);
-      }
-      printf("\n");
+    }
+    printf("\n");
 
-      printf("\n");
-      printf("duplicate values: ");
-      for(int i = 0; i < n; i++){
-        for(int j = i + 1; j < n; j++){
-            if(myArray[i] == myArray[j]){
-                counter++;
-                printf("%d ", myArray[i]);
-                break;
-            }
+    min = max = myArray[0];
+
+    for(int i = 1; i < n; i++){
+        if(min > myArray[i]){
+            min = myArray[i];
         }
-      }
+        if(max < myArray[i]){
+            max = myArray[i];
+        }
+    }
 
-      printf("\n");
-      printf("number of duplicate element : %d\n", counter);
+    printf("\nminimum element is : %d\n", min);
+    printf("maximum element is : %d\n", max);
+
+
+
+
+
+
 
 
     return 0;
